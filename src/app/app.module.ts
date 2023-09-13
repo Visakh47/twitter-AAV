1c0cb97172a04de8df881b5a3b2c2550e2b0f5cc
Visakh Bobby <vk_57@outlook.com>
2023-09-13 21:36:27 +0530

    styling fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { SidebarComponent } from './home/sidebar/sidebar.component';
-import { SettingListComponent } from './home/sidebar/setting-list/setting-list.component';
 import { HomeModule } from './home/home.module';
 
 @NgModule({
