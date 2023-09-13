556ff4666a404511839438a67cbea98ae440a80f
Visakh Bobby <vk_57@outlook.com>
2023-09-13 20:15:23 +0530

    home & sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './home/sidebar/sidebar.component';
+import { SettingListComponent } from './home/sidebar/setting-list/setting-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidebarComponent,
+    SettingListComponent
   ],
   imports: [
     BrowserModule,
