7aea5a7809352975073fad0b6f8b353310a6422d
Arjun <vvarjun.poduval@gmail.com>
2023-09-16 12:04:54 +0530

    signup -  converted to formgroup

--- src/app/signup-page/signup-page.component.ts
+++ src/app/signup-page/signup-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IUserDetails } from '../app.interface';
 import { Router } from '@angular/router';
 import { UserAuthService } from '../service/user-auth.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-page',
@@ -11,44 +12,45 @@ import { UserAuthService } from '../service/user-auth.service';
 export class SignupPageComponent {
   public error = false;
 
-  public form!: IUserDetails;
+  public signUpForm!: FormGroup<{
+    name: FormControl<any>,
+    email: FormControl<any>,
+    password: FormControl<any>,
+    confirmPassword: FormControl<any>
+
+  }>;
 
   
 //you can't directly access a service within a component
 //you have to perform dependency injection with a constructor to use router Service
 
   constructor(
-    public readonly router: Router,  private readonly userAuthService: UserAuthService
+    public readonly router: Router,private readonly fb: FormBuilder,  private readonly userAuthService: UserAuthService
   ) {
 
   }
   
   public ngOnInit(): void {
-    this.form = {
-      name :'',
-      email : '',
-      password : '',
-      confirmPassword : ''
-    }
+    this.signUpForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    })
   }
 
   public onReset(): void {
-    this.form = {
-      name :'',
-      email : '',
-      password : '',
-      confirmPassword : ''
-    }
+    this.signUpForm.reset();
   }
 
   public onSubmit(): void {
-    if(this.form.password === this.form.confirmPassword) {
-      this.error = false;
-      this.userAuthService.setUserDetails(this.form);
+
+    if(this.signUpForm.controls['password'].value===this.signUpForm.controls['confirmPassword'].value){
+      this.userAuthService.setUserDetails(this.signUpForm.value as IUserDetails);
       void this.router.navigate(['login']);
     }
-    else  
-      this.error = true;
+    else{
+      this.error=true;
+    }
   }
-
 }
